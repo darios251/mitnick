7592438338ab1b55a5f6e113e9b69ac2fed49458
lukasg04@gmail.com <lukasg04@gmail.com@2a62dd90-36c8-d724-a508-af843d5b17e8>
2012-04-02 22:44:44 +0000

    Refactorización de la capa de presentación

--- trunk/presentation/src/main/java/com/mitnick/presentation/view/ITitlePane.java
+++
@@ -1,5 +0,0 @@
-package com.mitnick.presentation.view;
-
-public interface ITitlePane {
-	public String getTitle();
-}
--- trunk/utils/src/main/java/com/mitnick/business/servicios/dtos/ServicioBaseDto.java
+++
@@ -1,9 +0,0 @@
-package com.mitnick.business.servicios.dtos;
-
-import java.io.Serializable;
-
-public class ServicioBaseDto implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
