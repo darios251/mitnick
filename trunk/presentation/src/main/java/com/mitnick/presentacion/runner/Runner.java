06a9223266474816649c95fbc54f1281a80c52b6
lukasg04@gmail.com <lukasg04@gmail.com@2a62dd90-36c8-d724-a508-af843d5b17e8>
2012-06-14 23:13:30 +0000

    Se agrega el objeto configuración y chequeo de seguridad de software legal.

--- trunk/presentation/src/main/java/com/mitnick/presentacion/runner/Runner.java
+++ trunk/presentation/src/main/java/com/mitnick/presentacion/runner/Runner.java
@@ -2,8 +2,11 @@ package com.mitnick.presentacion.runner;
 
 import org.apache.log4j.Logger;
 
+import com.mitnick.persistence.daos.IConfiguracionDAO;
+import com.mitnick.persistence.entities.Configuracion;
 import com.mitnick.presentacion.utils.DBInitialization;
 import com.mitnick.presentacion.vistas.PrincipalView;
+import com.mitnick.utils.DiskUtils;
 import com.mitnick.utils.PropertiesManager;
 import com.mitnick.utils.locator.BeanLocator;
 
@@ -12,7 +15,8 @@ public class Runner {
 	private static Logger logger = Logger.getLogger(Runner.class);
 	
 	public static void main(String[] args) {
-
+		
+		checkForRun();
 		logger.info("Iniciando la aplicación: Applicación del proyecto mitnick...");
 		logger.info("Inicializando el propertiesManager...");
 		BeanLocator.getBean("propertiesManager");
@@ -26,7 +30,21 @@ public class Runner {
 		principalView.setVisible(true);
 		
 		Thread.currentThread().setName(PropertiesManager.getProperty("application.name"));
-		
 	}
+
+	private static void checkForRun() {
+		IConfiguracionDAO configuracionDao = (IConfiguracionDAO) BeanLocator.getBean("configuracionDao");
+		
+		Configuracion configuracion = configuracionDao.getConfiguracion();
+		
+		if(!configuracion.getNumeroSerieDisco().equals(DiskUtils.getSerialNumber("C:"))) {
+			javax.swing.JOptionPane.showConfirmDialog((java.awt.Component) null,
+					"La copia del software no es legal.\nConsulte con los administradores de http://www.mitnick.com.ar", "Alerta!!",
+					javax.swing.JOptionPane.DEFAULT_OPTION);
+			System.exit(1);
+		}
+	}
+	
+	
 	
 }
