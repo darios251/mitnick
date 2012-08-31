67a0f0e37867a96ce70b2ee3a46b278810ddb9bc
lukasg04@gmail.com <lukasg04@gmail.com@2a62dd90-36c8-d724-a508-af843d5b17e8>
2012-08-31 19:53:05 +0000

    Fix de los números de secuencia después de compilar.

--- trunk/presentation/src/main/java/com/mitnick/presentacion/utils/DBInitialization.java
+++ trunk/presentation/src/main/java/com/mitnick/presentacion/utils/DBInitialization.java
@@ -4,7 +4,9 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.mitnick.persistence.daos.IConfiguracionDAO;
 import com.mitnick.persistence.daos.IProductoDAO;
+import com.mitnick.persistence.entities.Configuracion;
 import com.mitnick.persistence.entities.Producto;
 
 @Component("dbInitialization")
@@ -14,7 +16,27 @@ public class DBInitialization {
 	@Autowired
 	private IProductoDAO productoDao;
 	
+	@Autowired
+	private IConfiguracionDAO configuracionDao;
+	
 	public void initializeDB() {
+		// se hacen inserts para actualizar el número de secuencia
+		Configuracion configuracion = null;
+		
+		while (configuracion == null) {
+			configuracion = new Configuracion();
+			configuracion.setMagicValue("");
+			configuracion.setSalt("");
+			
+			try {
+				configuracion = configuracionDao.save(configuracion);
+			}
+			catch (Exception e) {
+				configuracion = null;
+			}
+		}
+		
+		configuracionDao.remove(configuracion.getId());
 		for(Producto producto : productoDao.getAll()) {
 			producto.setEliminado(false);
 			productoDao.saveOrUpdate(producto);
@@ -23,7 +45,4 @@ public class DBInitialization {
 		logger.info("La base de datos se ha inicializado con éxito.");
 	}
 
-	public void setProductoDao(IProductoDAO productoDao) {
-		this.productoDao = productoDao;
-	}
 }
