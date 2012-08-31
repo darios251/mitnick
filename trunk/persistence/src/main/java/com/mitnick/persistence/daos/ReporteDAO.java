72ab1e38e7dbd0820726cb25cb2cdf4149997458
agustinabm@gmail.com <agustinabm@gmail.com@2a62dd90-36c8-d724-a508-af843d5b17e8>
2012-08-31 14:14:15 +0000

    Creacion de presentacion y logica para mostrar reporte de venta de productos por dia

--- trunk/persistence/src/main/java/com/mitnick/persistence/daos/ReporteDAO.java
+++ trunk/persistence/src/main/java/com/mitnick/persistence/daos/ReporteDAO.java
@@ -13,6 +13,7 @@ import com.mitnick.persistence.entities.Producto;
 import com.mitnick.persistence.entities.ProductoVenta;
 import com.mitnick.persistence.entities.Venta;
 import com.mitnick.servicio.servicios.dtos.ReporteVentaArticuloDTO;
+import com.mitnick.servicio.servicios.dtos.ReporteVentasResultadoDTO;
 import com.mitnick.servicio.servicios.dtos.ReportesDto;
 
 @Repository("reporteDao")
@@ -27,23 +28,35 @@ public class ReporteDao extends GenericDaoHibernate<BaseObject, Serializable> im
 				Producto.class.getName() + " as p," + ProductoVenta.class.getName() + " as pv, " +
 				Venta.class.getName() + " as v where pv.producto.id=p.id and pv.venta.id=v.id " +
 				" group by p.codigo, p.descripcion, pv.cantidad, pv.precio, v.fecha ";
-			
 		
 		List<Object[]> items = getHibernateTemplate().find(hql);
 		List<ReporteVentaArticuloDTO> resultado = new ArrayList<ReporteVentaArticuloDTO>();
 		if (items!=null && !items.isEmpty()){
 			for (Object[] item: items){
-				ReporteVentaArticuloDTO dto = new ReporteVentaArticuloDTO();
-				dto.setProductoCodigo(item[0].toString());
-				dto.setProductoDescripcion(item[1].toString());
-				dto.setCantidad(Integer.parseInt(item[2].toString()));
-				dto.setTotal(Double.parseDouble(item[3].toString()));
-				dto.setFecha((Date)item[4]);
-				resultado.add(dto);
+				ReporteVentaArticuloDTO dto = getDTOFecha(resultado, item);
+				int cantidad = dto.getCantidad();
+				dto.setCantidad(cantidad + Integer.parseInt(item[2].toString()));
+				Double total = dto.getTotal();
+				dto.setTotal(total + Double.parseDouble(item[3].toString()));
 			}
 			
 		}
 		
 		return resultado;
+	}
+	
+	private ReporteVentaArticuloDTO getDTOFecha(List<ReporteVentaArticuloDTO> items, Object[] item){
+		for (ReporteVentaArticuloDTO dto: items){
+			if (dto.getFecha().equals((Date)item[4]) && dto.getProductoCodigo().equals(item[0].toString()))
+				return dto;
+		}		
+		ReporteVentaArticuloDTO dto = new ReporteVentaArticuloDTO();
+		dto.setProductoCodigo(item[0].toString());
+		dto.setProductoDescripcion(item[1].toString());
+		dto.setCantidad(0);
+		dto.setTotal(new Double(0));
+		dto.setFecha((Date)item[4]);
+		items.add(dto);
+		return dto;
 	}
 }
