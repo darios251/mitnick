94f76d7c5f8950ebd9d0880a19b59a932d439e74
agustinabm@gmail.com <agustinabm@gmail.com@2a62dd90-36c8-d724-a508-af843d5b17e8>
2013-06-15 16:43:56 +0000

    Filter search addeed

--- dac/domain-analysis-tool/app/util/FilterResultHelper.java
+++ dac/domain-analysis-tool/app/util/FilterResultHelper.java
@@ -1,9 +1,12 @@
 package util;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import org.hibernate.id.IdentityGenerator.GetGeneratedKeysDelegate;
 
 import models.OutputResult;
 
@@ -13,22 +16,437 @@ public class FilterResultHelper {
 			String refdomainedu, String extbacklinksgov, String refdomiansgov, String refdomainshome, String percenttoh, String orderBy, String order) {
 		OutputResult output = OutputResult.getInstance();
 		List<OutputDTO> results = output.getResult();
+		results = getInGoogle(results, inGoogle);
+		results = getWww(results, www);
+		results = getPr(results, pr);
+		results = getRefDomians(results, refDomians);
+		results = getRefips(results, refips);
+		results = getRefsubnet(results, refsubnet);
+		results = getExtlinksedu(results, extlinksedu);
+		results = getRefdomainedu(results, refdomainedu);
+		results = getExtbacklinksgov(results, extbacklinksgov);
+		results = getRefdomiansgov(results, refdomiansgov);
+		results = getRefdomainshome(results, refdomainshome);
+		results = getPercenttoh(results, percenttoh);
 		
-		if (orderBy == null)
-			orderBy = "site";	
-		if ("DESC".equals(order))
-			orderDESC(results, orderBy);
-		else
-			orderASC(results, orderBy);
+		if (results!=null && !results.isEmpty()){
+			if (orderBy == null)
+				orderBy = "site";	
+			if ("DESC".equals(order))
+				orderDESC(results, orderBy);
+			else
+				orderASC(results, orderBy);
+		}
 		
 		return results;
 	}
 
-	private BigDecimal greaterThan(String value){
+	private static List<OutputDTO> getInGoogle(List<OutputDTO> result, String inGoogle){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		if (inGoogle == null || "".equals(inGoogle) || "Select the option".equals(inGoogle))
+			filterResult = result;
+		else {
+			for (OutputDTO output: result){
+				if (inGoogle.equals(output.getInGoogle()))
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getWww(List<OutputDTO> result, String www){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		if (www == null || "".equals(www) || "Select the option".equals(www))
+			filterResult = result;
+		else {
+			for (OutputDTO output: result){
+				if (www.equals(output.getWww()))
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getPr(List<OutputDTO> result, String pr){
+		result = getGreatherPr(result, pr);
+		result = getLessPr(result, pr);
+		return result;
+	}
+	
+	private static List<OutputDTO> getGreatherPr(List<OutputDTO> result, String pr){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(pr);
+		if (greather == null)
+			return result;
+		
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getPr())<0)
+					filterResult.add(output);
+			}
+		}
+		
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getLessPr(List<OutputDTO> result, String pr){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(pr);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getPr())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getRefDomians(List<OutputDTO> result, String refDomians){
+		result = getGreatherRefDomians(result, refDomians);
+		result = getLessRefDomians(result, refDomians);
+		return result;
+	}
+	
+	private static List<OutputDTO> getGreatherRefDomians(List<OutputDTO> result, String refDomians){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(refDomians);				
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getRefDomians())<0)
+					filterResult.add(output);
+			}
+		}		
+		
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getLessRefDomians(List<OutputDTO> result, String refDomians){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(refDomians);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getRefDomians())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getRefips(List<OutputDTO> result, String refips){
+		result = getGreatherRefips(result, refips);
+		result = getLessRefips(result, refips);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherRefips(List<OutputDTO> result, String refips){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(refips);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getRefIps())<0)
+					filterResult.add(output);
+			}
+		}		
+		
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessRefips(List<OutputDTO> result, String refips){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(refips);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getRefIps())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getRefsubnet(List<OutputDTO> result, String refsubnet){
+		result = getGreatherRefsubnet(result, refsubnet);
+		result = getLessRefsubnet(result, refsubnet);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherRefsubnet(List<OutputDTO> result, String refsubnet){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(refsubnet);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getRefSubNet())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessRefsubnet(List<OutputDTO> result, String refsubnet){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(refsubnet);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getRefSubNet())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getExtlinksedu(List<OutputDTO> result, String extlinksedu){
+		result = getGreatherExtlinksedu(result, extlinksedu);
+		result = getLessExtlinksedu(result, extlinksedu);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherExtlinksedu(List<OutputDTO> result, String extlinksedu){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(extlinksedu);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getExtBackLnksEdu())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessExtlinksedu(List<OutputDTO> result, String extlinksedu){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(extlinksedu);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getExtBackLnksEdu())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getRefdomainedu(List<OutputDTO> result, String refdomainedu){
+		result = getGreatherRefdomainedu(result, refdomainedu);
+		result = getLessRefdomainedu(result, refdomainedu);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherRefdomainedu(List<OutputDTO> result, String refdomainedu){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(refdomainedu);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getRefDomainsEdu())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessRefdomainedu(List<OutputDTO> result, String refdomainedu){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(refdomainedu);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getRefDomainsEdu())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getExtbacklinksgov(List<OutputDTO> result, String extbacklinksgov){
+		result = getGreatherExtbacklinksgov(result, extbacklinksgov);
+		result = getLessExtbacklinksgov(result, extbacklinksgov);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherExtbacklinksgov(List<OutputDTO> result, String extbacklinksgov){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(extbacklinksgov);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getExtBackLinksGov())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessExtbacklinksgov(List<OutputDTO> result, String extbacklinksgov){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(extbacklinksgov);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getExtBackLinksGov())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getRefdomiansgov(List<OutputDTO> result, String refdomiansgov){
+		result = getGreatherRefdomiansgov(result, refdomiansgov);
+		result = getLessRefdomiansgov(result, refdomiansgov);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherRefdomiansgov(List<OutputDTO> result, String refdomiansgov){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(refdomiansgov);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getRefDomainsGov())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessRefdomiansgov(List<OutputDTO> result, String refdomiansgov){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(refdomiansgov);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getRefDomainsGov())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getRefdomainshome(List<OutputDTO> result, String refdomainshome){
+		result = getGreatherRefdomainshome(result, refdomainshome);
+		result = getLessRefdomainshome(result, refdomainshome);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherRefdomainshome(List<OutputDTO> result, String refdomainshome){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(refdomainshome);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getRefDomianHome())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessRefdomainshome(List<OutputDTO> result, String refdomainshome){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(refdomainshome);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getRefDomianHome())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static List<OutputDTO> getPercenttoh(List<OutputDTO> result, String percenttoh){
+		result = getGreatherPercenttoh(result, percenttoh);
+		result = getLessPercenttoh(result, percenttoh);
+		return result;
+	}
+	private static List<OutputDTO> getGreatherPercenttoh(List<OutputDTO> result, String percenttoh){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal greather = greaterThan(percenttoh);
+		if (greather == null)
+			return result;
+		if (greather!=null){
+			for (OutputDTO output: result){
+				if (greather.compareTo(output.getPercentToHome())<0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	private static List<OutputDTO> getLessPercenttoh(List<OutputDTO> result, String percenttoh){
+		List<OutputDTO> filterResult = new ArrayList<OutputDTO>();
+		BigDecimal less = lessThan(percenttoh);
+		if (less == null)
+			return result;
+		if (less!=null){
+			for (OutputDTO output: result){
+				if (less.compareTo(output.getPercentToHome())>0)
+					filterResult.add(output);
+			}
+		}
+		return filterResult;
+	}
+	
+	private static BigDecimal greaterThan(String value){
+		if (value==null)
+			return null;
+		String mayor = "";
+		int signoMayorIndex = value.indexOf(">");
+		signoMayorIndex++;
+		if (signoMayorIndex>=0){
+			boolean cont = true;
+			while (cont && signoMayorIndex<value.length()){				
+				char a = value.charAt(signoMayorIndex);
+				try {
+					new Integer(String.valueOf(a));
+					mayor = mayor.concat(String.valueOf(a));					
+				} catch (Exception e){
+					if ('.' == a || ','==a)
+						mayor = mayor.concat(".");		
+					else if ('>'==a || '<'==a)
+						cont = false;
+				}
+				signoMayorIndex++;
+				
+			}
+		}
+		if (!"".equals(mayor))
+			return new BigDecimal(mayor);
 		return null;
 	}
 
-	private BigDecimal lessThan(String value){
+	private static BigDecimal lessThan(String value){
+		if (value==null)
+			return null;
+		String mayor = "";
+		int signoMayorIndex = value.indexOf("<");
+		signoMayorIndex++;
+		if (signoMayorIndex>=0){
+			boolean cont = true;
+			while (cont && signoMayorIndex<value.length()){				
+				char a = value.charAt(signoMayorIndex);
+				try {
+					new Integer(String.valueOf(a));
+					mayor = mayor.concat(String.valueOf(a));					
+				} catch (Exception e){
+					if ('.' == a || ','==a)
+						mayor = mayor.concat(".");		
+					else if ('>'==a || '<'==a)
+						cont = false;
+				}
+				signoMayorIndex++;
+				
+			}
+		}
+		if (!"".equals(mayor))
+			return new BigDecimal(mayor);
 		return null;
 	}
 
