46a03421e9c21c6832d4223e1304659dc0ceed90
agustinabm@gmail.com <agustinabm@gmail.com@2a62dd90-36c8-d724-a508-af843d5b17e8>
2013-07-03 17:54:23 +0000

    Se agrega el numero de caja a las ventas - configurable en la apliacion

--- susmann-inst/datos/update03072013.sql
+++ susmann-inst/datos/update03072013.sql
@@ -9,10 +9,15 @@ CREATE TABLE vendedor
 )
 WITH (OIDS=FALSE);
 ALTER TABLE vendedor OWNER TO postgres;
-
-
 ALTER TABLE Venta
 ADD COLUMN vendedor_id bigint,
 ADD CONSTRAINT fk4eb7a2c21caf2d8 FOREIGN KEY (vendedor_id)
       REFERENCES vendedor (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE Venta
+ADD COLUMN numerocaja integer NULL;
+UPDATE Venta SET numerocaja=4;
+ALTER TABLE Venta ALTER COLUMN numerocaja SET NOT NULL;
+	  
+	  
